e1786e30d233d10ce3c88dd2246a15f450b382b5
Czeffik <trzewik93@gmail.com>
2021-01-07 19:26:42 +0100

    Add InformationEventPublisherIT

---
+++ src/test/integration/io/github/czeffik/events/infrastructure/app/information/InformationEventPublisherIT.groovy
@@ -0,0 +1,67 @@
+package io.github.czeffik.events.infrastructure.app.information
+
+import io.github.czeffik.events.domain.information.InformationEventPublisher
+import io.github.czeffik.events.domain.information.events.InformationEvent
+import io.github.czeffik.events.domain.information.events.InformationEventCreation
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
+import spock.mock.DetachedMockFactory
+
+@ActiveProfiles(['test'])
+@SpringBootTest(
+    classes = [
+        InformationEventPublisherConfiguration,
+        InformationEventPublisherIT.TestConfig
+    ]
+)
+class InformationEventPublisherIT extends Specification implements InformationEventCreation {
+
+    @Autowired
+    ApplicationEventPublisher applicationEventPublisher
+    @Autowired
+    InformationEventPublisher informationEventPublisher
+
+    def 'should publish information event'() {
+        given:
+            def event = createWithPriceEvent()
+        when:
+            informationEventPublisher.publish(event)
+        then:
+            1 * applicationEventPublisher.publishEvent(event)
+    }
+
+    def 'should publish multiple information events'() {
+        given:
+            def events = [
+                createWithPriceEvent(),
+                createFakeEvent(),
+                createUpdateReceivedEvent()
+            ]
+        when:
+            informationEventPublisher.publish(events)
+        then:
+            events.size() * applicationEventPublisher.publishEvent({ InformationEvent event ->
+                assert events.contains(event)
+                return event
+            })
+    }
+
+
+    //TODO: Consider doing it somehow better? Maybe create test listener?
+    @TestConfiguration
+    static class TestConfig {
+        DetachedMockFactory factory = new DetachedMockFactory()
+
+        @Bean
+        @Primary
+        ApplicationEventPublisher applicationEventPublisher() {
+            return factory.Mock(ApplicationEventPublisher)
+        }
+    }
+}
