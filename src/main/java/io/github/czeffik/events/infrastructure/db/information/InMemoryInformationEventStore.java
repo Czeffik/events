18c01d82de1a9683e9ac761e99a93f1e46b5e2ba
Czeffik <trzewik93@gmail.com>
2021-01-07 19:41:34 +0100

    Add InMemoryInformationEventStore

---
+++ src/main/java/io/github/czeffik/events/infrastructure/db/information/InMemoryInformationEventStore.java
@@ -0,0 +1,17 @@
+package io.github.czeffik.events.infrastructure.db.information;
+
+import io.github.czeffik.events.domain.information.InformationEventStore;
+import io.github.czeffik.events.domain.information.events.InformationEvent;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+class InMemoryInformationEventStore implements InformationEventStore {
+
+    private final Map<String, InformationEvent> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void store(InformationEvent event) {
+        this.store.put(event.getEventId(), event);
+    }
+}
